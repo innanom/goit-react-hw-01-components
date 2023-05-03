5d0ebf2f3c94f296129aeae6a2645f73ace40f09
Inna Nomerovkina <inkosof@gmail.com>
2023-05-04 00:25:57 +0300

    FriendList-component

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,15 +1,17 @@
 import PropTypes from 'prop-types';
+import { FriendListItem } from './FriendListItem';
 import css from './FriendList.module.css'
 
 export const FriendList = ({ friends }) => {
   return (
       <ul className={css.friend_list}>
       {friends.map(({ avatar, name, isOnline, id }) => (
-        <li className={css.item }key={id}>
-              <span className={`${css.status} ${css[isOnline]}`}></span>
-          <img className="avatar" src={avatar} alt="User avatar" width="48" />
-          <p className="name">{name}</p>
-        </li>
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
       ))}
     </ul>
   );
